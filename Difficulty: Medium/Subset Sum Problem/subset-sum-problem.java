a71c0c082434eb2ce7b6da2a236935e24d04bb9d
Code <samirbhandari1231@gmail.com>
2026-01-19 10:56:19 +0545

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -1,36 +1,21 @@
 class Solution {
-
     static Boolean isSubsetSum(int arr[], int sum) {
-        // code here
-        int[][] dp=new int[arr.length][sum+1];
-        for(int i=0;i<arr.length;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return helper(arr,sum,0,dp);
-        
-        
+        boolean[][] dp=new boolean[arr.length][sum];
+    return helper(arr,sum,0,dp);
     }
-    public static boolean helper(int arr[],int target,int idx,int[][] dp){
-        if(idx>=arr.length || target==0){
-            if(target==0)return true;
-            return false;
-        }
-        if(dp[idx][target]!=-1){
-            if(dp[idx][target]==1)return true;
-            return false;
-        }
+    public static boolean helper(int[] arr,int target,int idx, boolean[][] dp){
+        if(target==0)return true;
+        if(idx>=arr.length)return false;
+        
+        if(dp[idx][target-1])return false;
+       
+        boolean notpick=helper(arr,target,idx+1,dp);
         boolean pick=false;
         if(arr[idx]<=target){
-             pick=helper(arr,target-arr[idx],idx+1,dp);
+            pick=helper(arr,target-arr[idx],idx+1,dp);
         }
+         dp[idx][target-1]=true;
      
-        boolean notpick=helper(arr,target,idx+1,dp);
-        if(pick || notpick){
-           dp[idx][target]=1; 
-        }else{
-             dp[idx][target]=0;  
-        }
-        return pick || notpick ;
-        
+        return pick || notpick;
     }
 }
